5286400718700f9ae21f87866ad18999130c9906
root <root@ip-172-31-17-188.eu-central-1.compute.internal>
2022-01-03 10:08:04 +0000

    task done

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(suhlob.hopto.org www.suhlob.hopto.org)
+domains=(docker312.ddns.net)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="nikolay.suglob@gmail.com" # Adding a valid address is strongly recommended
--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ echo "Started building nginx and apache images."
 docker build --tag my-nginx:latest ./data/nginx
 docker build --tag my-apache:latest ./data/apache
 
-echo "Started creating nginx and apache containers."
-docker run -p 80:80 -d --rm --name my-nginx my-nginx:latest
-docker run -p 8080:80 -d --rm --name my-apache my-apache:latest
+#echo "Started creating nginx and apache containers."
+#docker run -p 80:80 -d --rm --name my-nginx my-nginx:latest
+#docker run -p 8080:80 -d --rm --name my-apache my-apache:latest
