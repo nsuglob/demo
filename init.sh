d86f8587cad3888e36abcac98daa6369b56b8d60
Mikalai Suhlob <nikolay.suglob@gmail.com>
2022-01-10 14:38:03 +0300

    changed default apache port

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ docker build --tag my-apache:latest ./data/apache
 
 #echo "Started creating nginx and apache containers."
 #docker run -p 80:80 -d --rm --name my-nginx my-nginx:latest
-#docker run -p 8080:80 -d --rm --name my-apache my-apache:latest
+#docker run -p 8081:80 -d --rm --name my-apache my-apache:latest
